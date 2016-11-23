af6f733611018acbc14d5df3e73ae109d2274039
Madison Kawakami <madi.kawakami@gmail.com>
2016-11-23 09:12:36 -1000

    test

---
+++ test_review.js
@@ -0,0 +1,42 @@
+// Madison Kawakami
+// 11/23/16
+// title: javascript test review
+
+var knight = "Arthur";
+var maiden = "Jane";
+var monster = "Dragon";
+
+var weaponChest = "Mace", "Sorcerer's Stone", "Excalibur";
+
+var hero 
+var wizard = "Merlin";
+var weapon = "Mace";
+var power = "45";
+
+
+function swordInTheStone ("worthy");
+	if ("Arthur" == worthy); {
+		console.log ("We have found our King worthy");
+}
+	
+	else ("Arthur" = worthy) {
+		console.log ("We must keep searching, for worthy is unworthy.");
+}
+
+function trueLove ("King", "Queen");
+	if ("Guinevere" == Queen);
+		( "Arthur " == King); {
+			console.log ( "On this day, our" + parameter1 + "and" + parameter2 + " shall rule happily ever after");
+		}
+	else {
+		console.log ( " we must keep searching, for true love must reign over Camelot");
+	}
+
+function epicBattle ("wizard", "enemy", "power");
+	if (power = >16 );
+		(power = < 40); {
+			console.log ( parameter1 + "has almost slain the" + "enemy.");
+		}
+	else {
+		console.log ("Our" + parameter1 +"has slain the" +parameter2 + "!");
+	}
